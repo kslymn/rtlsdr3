e2ae5b765ca7b2e8cbff49b1ddebe831c0edd1aa
kslymn <kasuleyman@gmail.com>
2015-04-04 16:56:45 +0300

    Update freqshow.py

--- freqshow.py
+++ freqshow.py
@@ -33,7 +33,7 @@ import ui
 
 
 # Application configuration.
-SDR_SAMPLE_SIZE = 1024	# Number of samples to grab from the radio.  Should be
+SDR_SAMPLE_SIZE = 2048	# Number of samples to grab from the radio.  Should be
 						# larger than the maximum display width.
 
 CLICK_DEBOUNCE  = 0.4	# Number of seconds to wait between clicks events. Set
